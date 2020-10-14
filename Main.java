5b48d6ce1d37aa830fa6d551edcc342d445f29f3
chadmorris55 <71134162+chadmorris55@users.noreply.github.com>
2020-10-14 09:04:29 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,12 +4,37 @@
  */
 public class Main {
 
+
+  public static boolean isNiven(int number){
+  //variables here
+  int last;
+  int storage = 1;
+  //so i can use the original number later
+  int numberTwo = number;
+  boolean check;
+  //modulus number by 10 and adds the remainder to the storage variable then divides the number by 10
+    while(number > 10){
+      last = number % 10;
+      storage = storage + last;
+      number = number / 10;
+      }
+    //checks if the original number divided by the final value of storage is equal to 0, if so its a niven number so its true if not, its false
+    if(numberTwo % storage == 0){
+      check = true;
+    } else {
+      check = false;
+    }
+    //return
+    return check;
+  }
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
     // TODO code application logic here
-    
+    //calls method
+    boolean endNum = isNiven(18);
+    System.out.println(endNum);
   }
 }
